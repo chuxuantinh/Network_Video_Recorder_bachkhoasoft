b5640e238cb64edff35154ada2f51680628f5065
Chu Xuân Tình <50776038+chuxuantinh@users.noreply.github.com>
2020-09-13 14:05:59 +0700

    Initial commit

---
+++ Network_Video_Recorder_bachkhoasoft/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Network_Video_Recorder01
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.GroupBox_Connect = new System.Windows.Forms.GroupBox();
+            this.button_Connect = new System.Windows.Forms.Button();
+            this.CameraBox = new System.Windows.Forms.GroupBox();
+            this.button_CaptureVideo1Stop = new System.Windows.Forms.Button();
+            this.TextBox_SaveTo = new System.Windows.Forms.TextBox();
+            this.button_SaveTo1 = new System.Windows.Forms.Button();
+            this.button_CaptureVideo1Start = new System.Windows.Forms.Button();
+            this.GroupBox_Capture = new System.Windows.Forms.GroupBox();
+            this.GroupBox_Connect.SuspendLayout();
+            this.GroupBox_Capture.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GroupBox_Connect
+            // 
+            this.GroupBox_Connect.Controls.Add(this.button_Connect);
+            this.GroupBox_Connect.Location = new System.Drawing.Point(10, 10);
+            this.GroupBox_Connect.Name = "GroupBox_Connect";
+            this.GroupBox_Connect.Size = new System.Drawing.Size(110, 60);
+            this.GroupBox_Connect.TabIndex = 0;
+            this.GroupBox_Connect.TabStop = false;
+            this.GroupBox_Connect.Text = "Connect";
+            // 
+            // button_Connect
+            // 
+            this.button_Connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button_Connect.ForeColor = System.Drawing.Color.Black;
+            this.button_Connect.Location = new System.Drawing.Point(10, 20);
+            this.button_Connect.Name = "button_Connect";
+            this.button_Connect.Size = new System.Drawing.Size(90, 25);
+            this.button_Connect.TabIndex = 6;
+            this.button_Connect.Text = "Connect";
+            this.button_Connect.UseVisualStyleBackColor = true;
+            this.button_Connect.Click += new System.EventHandler(this.button_Connect_Click);
+            // 
+            // CameraBox
+            // 
+            this.CameraBox.Location = new System.Drawing.Point(10, 90);
+            this.CameraBox.Name = "CameraBox";
+            this.CameraBox.Size = new System.Drawing.Size(290, 210);
+            this.CameraBox.TabIndex = 3;
+            this.CameraBox.TabStop = false;
+            this.CameraBox.Text = "Live camera ";
+            // 
+            // button_CaptureVideo1Stop
+            // 
+            this.button_CaptureVideo1Stop.Location = new System.Drawing.Point(190, 20);
+            this.button_CaptureVideo1Stop.Name = "button_CaptureVideo1Stop";
+            this.button_CaptureVideo1Stop.Size = new System.Drawing.Size(90, 25);
+            this.button_CaptureVideo1Stop.TabIndex = 36;
+            this.button_CaptureVideo1Stop.Text = "Stop capture";
+            this.button_CaptureVideo1Stop.UseVisualStyleBackColor = true;
+            this.button_CaptureVideo1Stop.Click += new System.EventHandler(this.Button_CaptureVideoStop_Click);
+            // 
+            // TextBox_SaveTo
+            // 
+            this.TextBox_SaveTo.Location = new System.Drawing.Point(140, 60);
+            this.TextBox_SaveTo.Name = "TextBox_SaveTo";
+            this.TextBox_SaveTo.Size = new System.Drawing.Size(140, 20);
+            this.TextBox_SaveTo.TabIndex = 35;
+            // 
+            // button_SaveTo1
+            // 
+            this.button_SaveTo1.Location = new System.Drawing.Point(10, 60);
+            this.button_SaveTo1.Name = "button_SaveTo1";
+            this.button_SaveTo1.Size = new System.Drawing.Size(90, 25);
+            this.button_SaveTo1.TabIndex = 34;
+            this.button_SaveTo1.Text = "Save to:";
+            this.button_SaveTo1.UseVisualStyleBackColor = true;
+            this.button_SaveTo1.Click += new System.EventHandler(this.Button_SaveTo_Click);
+            // 
+            // button_CaptureVideo1Start
+            // 
+            this.button_CaptureVideo1Start.Location = new System.Drawing.Point(10, 20);
+            this.button_CaptureVideo1Start.Name = "button_CaptureVideo1Start";
+            this.button_CaptureVideo1Start.Size = new System.Drawing.Size(90, 25);
+            this.button_CaptureVideo1Start.TabIndex = 33;
+            this.button_CaptureVideo1Start.Text = "Start capture";
+            this.button_CaptureVideo1Start.UseVisualStyleBackColor = true;
+            this.button_CaptureVideo1Start.Click += new System.EventHandler(this.Button_CaptureVideoStart_Click);
+            // 
+            // GroupBox_Capture
+            // 
+            this.GroupBox_Capture.Controls.Add(this.TextBox_SaveTo);
+            this.GroupBox_Capture.Controls.Add(this.button_CaptureVideo1Stop);
+            this.GroupBox_Capture.Controls.Add(this.button_CaptureVideo1Start);
+            this.GroupBox_Capture.Controls.Add(this.button_SaveTo1);
+            this.GroupBox_Capture.Location = new System.Drawing.Point(10, 300);
+            this.GroupBox_Capture.Name = "GroupBox_Capture";
+            this.GroupBox_Capture.Size = new System.Drawing.Size(290, 100);
+            this.GroupBox_Capture.TabIndex = 0;
+            this.GroupBox_Capture.TabStop = false;
+            this.GroupBox_Capture.Text = "Capture";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 414);
+            this.Controls.Add(this.GroupBox_Capture);
+            this.Controls.Add(this.CameraBox);
+            this.Controls.Add(this.GroupBox_Connect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Network Video Recorder";
+            this.GroupBox_Connect.ResumeLayout(false);
+            this.GroupBox_Capture.ResumeLayout(false);
+            this.GroupBox_Capture.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.GroupBox GroupBox_Connect;
+        private System.Windows.Forms.Button button_Connect;
+        private System.Windows.Forms.GroupBox CameraBox;
+        private System.Windows.Forms.Button button_CaptureVideo1Stop;
+        private System.Windows.Forms.TextBox TextBox_SaveTo;
+        private System.Windows.Forms.Button button_SaveTo1;
+        private System.Windows.Forms.Button button_CaptureVideo1Start;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.GroupBox GroupBox_Capture;
+    }
+}
---
+++ Network_Video_Recorder_bachkhoasoft/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Ozeki.Media.IPCamera;
+using Ozeki.Media.MediaHandlers.Video;
+using Ozeki.Media.Video.Controls;
+using Ozeki.Media.MediaHandlers;
+// chutinh - https://bachkhoasoft.com
+
+namespace Network_Video_Recorder01
+{
+    public partial class Form1 : Form
+    {
+        private IIPCamera _camera;
+        private DrawingImageProvider _imageProvider;
+        private MediaConnector _connector;
+        private VideoViewerWF _videoViewerWf;
+        private MPEG4Recorder _mpeg4Recorder;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _imageProvider = new DrawingImageProvider();
+            _connector = new MediaConnector();
+            _videoViewerWf = new VideoViewerWF();
+            SetVideoViewer();
+        }
+
+        private void SetVideoViewer()
+        {
+            CameraBox.Controls.Add(_videoViewerWf);
+            _videoViewerWf.Size = new Size(260, 180);
+            _videoViewerWf.BackColor = Color.Black;
+            _videoViewerWf.TabStop = false;
+            _videoViewerWf.Location = new Point(14, 19);
+            _videoViewerWf.Name = "_videoViewerWf";
+        }
+
+        private void button_Connect_Click(object sender, EventArgs e)
+        {
+            _camera = IPCameraFactory.GetCamera("192.168.115.175:8080", "admin", "admin");
+            _connector.Connect(_camera.VideoChannel, _imageProvider);
+            _videoViewerWf.SetImageProvider(_imageProvider);
+            _videoViewerWf.Start();
+            _camera.Start();
+        }
+
+        private void Button_CaptureVideoStart_Click(object sender, EventArgs e)
+        {
+            var path = TextBox_SaveTo.Text;
+            if (!String.IsNullOrEmpty(path))
+                StartVideoCapture(path);
+        }
+
+        private void StartVideoCapture(string path)
+        {
+            var date = DateTime.Now.Year + "y-" + DateTime.Now.Month + "m-" + DateTime.Now.Day + "d-" +
+                       DateTime.Now.Hour + "h-" + DateTime.Now.Minute + "m-" + DateTime.Now.Second + "s";
+            string currentpath;
+            if (String.IsNullOrEmpty(path))
+                currentpath = date + ".mp4";
+            else
+                currentpath = path + "\\" + date + ".mp4";
+
+            _mpeg4Recorder = new MPEG4Recorder(currentpath);
+            _mpeg4Recorder.MultiplexFinished += Mpeg4Recorder_MultiplexFinished;
+            _connector.Connect(_camera.AudioChannel, _mpeg4Recorder.AudioRecorder);
+            _connector.Connect(_camera.VideoChannel, _mpeg4Recorder.VideoRecorder);
+        }
+
+        private void Mpeg4Recorder_MultiplexFinished(object sender, Ozeki.VoIP.VoIPEventArgs<bool> e)
+        {
+            _connector.Disconnect(_camera.AudioChannel, _mpeg4Recorder.AudioRecorder);
+            _connector.Disconnect(_camera.VideoChannel, _mpeg4Recorder.VideoRecorder);
+            _mpeg4Recorder.Dispose();
+        }
+
+        private void Button_CaptureVideoStop_Click(object sender, EventArgs e)
+        {
+            StopVideoCapture();
+        }
+
+        private void StopVideoCapture()
+        {
+            _mpeg4Recorder.Multiplex();
+            _connector.Disconnect(_camera.AudioChannel, _mpeg4Recorder.AudioRecorder);
+            _connector.Disconnect(_camera.VideoChannel, _mpeg4Recorder.VideoRecorder);
+        }
+
+        private void Button_SaveTo_Click(object sender, EventArgs e)
+        {
+            var result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+                TextBox_SaveTo.Text = folderBrowserDialog1.SelectedPath;
+        }
+    }
+}
